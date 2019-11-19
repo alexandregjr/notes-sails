b6914286d91402b09599ec507b9e3bde28dd3d83
alexandregjr <alexandregalochajr@gmail.com>
2019-11-19 20:57:57 -0300

    fixed issues

--- assets/js/Tag.js
+++ assets/js/Tag.js
@@ -5,8 +5,8 @@ Vue.component("Tag", {
             this.$emit("remove-tag", this.id)
         }
     },
-    template:  `<div class="flex bg-yellow-500 rounded-full px-4 mr-2 mt-2">
-                    <span class="text-yellow-900"> {{ this.name }} </span>
+    template:  `<div class="flex bg-yellow-500 px-4 mr-2 mt-2" :class="this.name.length > 28 ? 'rounded-lg' : 'rounded-full'">
+                    <span class="text-yellow-900 break-all"> {{ this.name }} </span>
                     <span class="mx-1"></span>
                     <button v-on:click="emitRemoveTag" class="italic text-yellow-900">X</button>
                 </div>`
